577bca69fd9e3a1f4de27a3c993a96a1a81337bd
david <davmwa90@gmail.com>
2024-01-20 20:59:08 +0300

    added contact element in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import About from './components/About'
 import Skills from './components/Skills'
+import Contact from './components/Contact'
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false)
+    const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    return storedDarkMode ? JSON.parse(storedDarkMode) : false;
+  });
+
+ 
+  const setDarkModePreference = (value) => {
+    setDarkMode(value);
+    localStorage.setItem("darkMode", JSON.stringify(value));
+  };
 
 // Dark mode toggling 
 const handleDarkToggle = () => {
-  setDarkMode(!darkMode)
+   setDarkModePreference(!darkMode)
 }
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", darkMode);
+  }, [darkMode]);
 
 
   return (
@@ -20,6 +34,7 @@ const handleDarkToggle = () => {
         <Home />
         <About />
         <Skills />
+        <Contact />
       </div>
     </div>
   )
